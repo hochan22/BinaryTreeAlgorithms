030e8fa091469abe2182808fd2c587b5ef0ea99a
Hao Chen <haochenx22@gmail.com>
2018-05-17 22:48:52 -0700

    second commit

--- Project/StandardBinaryTree.java
+++ Project/StandardBinaryTree.java
@@ -4,5 +4,18 @@ import java.util.ArrayList;
 
 public class StandardBinaryTree
 {
+    private TreeNode root;
+
+    /**
+     * constructor of the Standard Binary Tree with a root tree node
+     * @param value is the value of the root to split the subtrees
+     */
+    public StandardBinaryTree(int value)
+    {
+        TreeNode treeNode = new TreeNode(value);
+        this.root = treeNode;
+    }
+
+    public void createTree()
 
 }
